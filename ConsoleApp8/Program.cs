773e764dd574acfc58650199b7163e157e550cfd
RustemovTural <tu80rq7h6@code.edu.az>
2024-03-17 19:04:27 +0400

    metod task

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        public static void Firstletter(string sentence)
+        {
+            char letter = ' ';
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (letter == ' ')
+                {
+                    Console.WriteLine(sentence[i]);
+                }
+                letter = sentence[i];
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string sentence = " hello world";
+            Firstletter(sentence);
+        }
+    }
+}
+    
+
